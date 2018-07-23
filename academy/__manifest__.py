1696d7b363d010c2ada27e2fedf5260699345024
Carlos Joel <cj@carlosjoel.net>
2018-07-23 16:33:29 -0500

    Initial website module

---
+++ academy/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Academy',
+    'summary': """Website Module""",
+    'author': 'CJ',
+    'website': 'https://carlosjoel.net',
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
